c8be1e77a3a9c0c296d6899d2a50f02c27e2e638
Tim Hazlett <timhazlett2016@gmail.com>
2026-03-31 09:38:40 -0500

    Adding some baseline network error handling code

--- packages/network-util/src/hooks/suspense-query-deferred.ts
+++ packages/network-util/src/hooks/suspense-query-deferred.ts
@@ -14,32 +14,32 @@ import { useDeepCompareMemo } from 'use-deep-compare';
  * @param spinDelayOptions These optional options are provided to spinDelay
  */
 export function useSuspenseQueryDeferred<
-    TQueryFunctionData = unknown,
-    TError = DefaultError,
-    TData = TQueryFunctionData,
-    TQueryKey extends QueryKey = QueryKey,
+  TQueryFunctionData = unknown,
+  TError = DefaultError,
+  TData = TQueryFunctionData,
+  TQueryKey extends QueryKey = QueryKey,
 >(
-    options: UseSuspenseQueryOptions<TQueryFunctionData, TError, TData, TQueryKey>,
-    spinDelayOptions?: Partial<typeof defaultOptions>,
+  options: UseSuspenseQueryOptions<TQueryFunctionData, TError, TData, TQueryKey>,
+  spinDelayOptions?: Partial<typeof defaultOptions>,
 ): {
-    isSuspending: boolean;
-    query: UseSuspenseQueryResult<TData, TError>;
+  isSuspending: boolean;
+  query: UseSuspenseQueryResult<TData, TError>;
 } {
-    const queryKey = useDeepCompareMemo(() => {
-        return options.queryKey;
-    }, [options.queryKey]);
+  const queryKey = useDeepCompareMemo(() => {
+    return options.queryKey;
+  }, [options.queryKey]);
 
-    const deferredQueryKey = useDeferredValue(queryKey);
+  const deferredQueryKey = useDeferredValue(queryKey);
 
-    const query = useSuspenseQuery({
-        ...options,
-        queryKey: deferredQueryKey,
-    });
+  const query = useSuspenseQuery({
+    ...options,
+    queryKey: deferredQueryKey,
+  });
 
-    const isSuspending = useSpinDelay(queryKey !== deferredQueryKey, spinDelayOptions);
+  const isSuspending = useSpinDelay(queryKey !== deferredQueryKey, spinDelayOptions);
 
-    return {
-        isSuspending,
-        query,
-    };
+  return {
+    isSuspending,
+    query,
+  };
 }
---
+++ packages/network-util/src/network/index.ts
@@ -0,0 +1,6 @@
+export * from './network-error';
+export * from './network-error-validation';
+export * from './network-error-with-message-list';
+
+export * from './check-response-message-for-error';
+export * from './refine-network-response';
