e85b7057ec87760e66fa658038b6b104f2fa8eec
Tim Hazlett <47572900+thazlett16@users.noreply.github.com>
2026-03-31 16:26:48 -0500

    feat: adding additional network schema (#6)
    * Adding new package and dependencies
    
    * Adding hook source code
    
    * Updating config
    
    * Adding some baseline schema utils for service validation
    
    * Adding a base-entity
    
    * Adding README.md for network project
    
    * Adding some new network schemas
    
    * Update lock
    
    * Updating deps

---
+++ packages/network-util/src/valibot/entity/active-entity/schema.ts
@@ -0,0 +1,12 @@
+import * as v from 'valibot';
+
+import { instant } from '#src/valibot/schema/instant/schema';
+import { responseNullable } from '#src/valibot/schema/nullable/schema';
+
+export const activeEntity = v.object({
+  active_at_timestamp: instant,
+  expired_at_timestamp: responseNullable(instant),
+});
+
+// type InputActiveEntity = v.InferInput<typeof activeEntity>
+// type OutputActiveEntity = v.InferOutput<typeof activeEntity>
---
+++ packages/network-util/src/valibot/entity/extended-audit-entity/schema.ts
@@ -0,0 +1,13 @@
+import * as v from 'valibot';
+
+import { responseNullable } from '#src/valibot/schema/nullable/schema';
+
+export const extendedAuditEntity = v.object({
+  created_by_process: responseNullable(v.string()),
+  created_by_user_id: responseNullable(v.string()),
+  updated_by_process: responseNullable(v.string()),
+  updated_by_user_id: responseNullable(v.string()),
+});
+
+// type InputExtendedAuditEntity = v.InferInput<typeof extendedAuditEntity>
+// type OutputExtendedAuditEntity = v.InferOutput<typeof extendedAuditEntity>
