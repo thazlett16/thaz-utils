999f694e3b3b4b89780e0dafc8647d13623737a8
Tim Hazlett <timhazlett2016@gmail.com>
2026-04-01 09:35:58 -0500

    Copy issue

--- packages/form-dayjs-util/src/actions/index.ts
+++ packages/form-dayjs-util/src/actions/index.ts
@@ -1,6 +1,6 @@
 export { when } from '@thazstack/form-util';
 
-0.1.0export * from './to-instant-value/action';
+export * from './to-instant-value/action';
 export * from './to-plain-date-time-value/action';
 export * from './to-plain-date-value/action';
 export * from './to-plain-time-value/action';
