b1909b28874b03099a393c8672fe1dc9f039b2b5
Tim Hazlett <timhazlett2016@gmail.com>
2026-04-07 12:04:13 -0500

    Adding some new values to hooks

--- packages/form-util/src/hooks/normalize-field-value-plain-date.ts
+++ packages/form-util/src/hooks/normalize-field-value-plain-date.ts
@@ -1,18 +1,20 @@
+import type * as v from 'valibot';
+
 import { Temporal } from '@js-temporal/polyfill';
 import { useStore } from '@tanstack/react-form';
 import { useMemo } from 'react';
 
+import type { _plainDateNullable } from '#src/schemas/plain-date/schema';
+
+import { FormConversionError } from '#src/error';
 import { useFieldContext } from '#src/tanstack-form.config';
 
-export type FieldValuePlainDate =
-  | Temporal.ZonedDateTime
-  | Temporal.PlainDateTime
-  | Temporal.PlainDate
-  | null
-  | undefined;
+type SchemaType = v.InferInput<ReturnType<typeof _plainDateNullable>>;
+
+export type FieldValuePlainDate = Exclude<SchemaType, string>;
 
 export function useNormalizeFieldValuePlainDate() {
-  const field = useFieldContext<FieldValuePlainDate>();
+  const field = useFieldContext<SchemaType>();
 
   const baseFieldValue = useStore(field.store, (state) => state.value);
 
@@ -27,12 +29,24 @@ export function useNormalizeFieldValuePlainDate() {
       }
     } catch (error: unknown) {
       console.error('useNormalizeFieldValuePlainDate - Failed to normalize value', error);
-      throw new Error('useNormalizeFieldValuePlainDate - Failed to normalize value', { cause: error });
+      throw new FormConversionError({
+        data: baseFieldValue,
+        message: 'useNormalizeFieldValuePlainDate - Failed to normalize value',
+      });
+    }
+
+    if (typeof baseFieldValue === 'string') {
+      throw new FormConversionError({
+        data: baseFieldValue,
+        message: 'useNormalizeFieldValuePlainDate - Convert from string before passing into form',
+      });
     }
 
     if (!(baseFieldValue === null || baseFieldValue === undefined)) {
-      console.error('useNormalizeFieldValuePlainDate - Invalid type in context:', baseFieldValue);
-      throw new Error('useNormalizeFieldValuePlainDate - Invalid type in context');
+      throw new FormConversionError({
+        data: baseFieldValue,
+        message: 'useNormalizeFieldValuePlainDate - Invalid type in context',
+      });
     }
 
     return null;
