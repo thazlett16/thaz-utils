4c7866ad9a535a4a3efea0149d6c9d41d6045ff7
Tim Hazlett <timhazlett2016@gmail.com>
2026-04-08 14:54:58 -0500

    Number normalization with string included

--- packages/form-util/src/hooks/normalize-field-value-number.ts
+++ packages/form-util/src/hooks/normalize-field-value-number.ts
@@ -5,32 +5,40 @@ import { useMemo } from 'react';
 
 import type { _numberNullable } from '#src/schemas/number/schema';
 
-import { FormConversionError } from '#src/error';
+import { FormConversionError, FormTypeError } from '#src/error';
 import { useFieldContext } from '#src/tanstack-form.config';
 
-type SchemaType = v.InferInput<ReturnType<typeof _numberNullable>>;
+type FieldValueNumberSchemaType = v.InferInput<ReturnType<typeof _numberNullable>>;
 
-export type FieldValueNumber = Exclude<SchemaType, string>;
+export type FieldValueNumber = Exclude<FieldValueNumberSchemaType, string>;
 
 export function useNormalizeFieldValueNumber() {
-  const field = useFieldContext<SchemaType>();
+  const field = useFieldContext<FieldValueNumberSchemaType>();
 
   const baseFieldValue = useStore(field.store, (state) => state.value);
 
   return useMemo<number | null>(() => {
     if (typeof baseFieldValue === 'number') {
       return baseFieldValue;
-    }
+    } else if (typeof baseFieldValue === 'string') {
+      if (baseFieldValue.trim() === '') {
+        return null;
+      }
 
-    if (typeof baseFieldValue === 'string') {
-      throw new FormConversionError({
-        data: baseFieldValue,
-        message: 'useNormalizeFieldValueNumber - Convert from string before passing into form',
-      });
+      try {
+        return Number(baseFieldValue);
+      } catch (error: unknown) {
+        throw new FormConversionError(
+          {
+            message: 'useNormalizeFieldValueNumber - Failed to normalize value',
+          },
+          { cause: error },
+        );
+      }
     }
 
     if (!(baseFieldValue === null || baseFieldValue === undefined)) {
-      throw new FormConversionError({
+      throw new FormTypeError({
         data: baseFieldValue,
         message: 'useNormalizeFieldValueNumber - Invalid type in context',
       });
