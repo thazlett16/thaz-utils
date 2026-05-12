51a06c460ab24067967c9ac5dc618fc1c184c5cf
Tim Hazlett <timhazlett2016@gmail.com>
2026-05-12 17:19:34 -0500

    Adding test directory and config

--- packages/form-util/vite.config.ts
+++ packages/form-util/vite.config.ts
@@ -2,6 +2,8 @@ import vitePluginReact from '@vitejs/plugin-react';
 import { externalizeDeps } from 'vite-plugin-externalize-deps';
 import { defineConfig } from 'vite-plus';
 
+import { playwright } from 'vite-plus/test/browser-playwright';
+
 export default defineConfig({
   run: {
     tasks: {
@@ -9,8 +11,12 @@ export default defineConfig({
         command: 'vp pack',
         dependsOn: ['@thazstack/temporal-util#build', '@thazstack/temporal-valibot-util#build'],
       },
-      typecheck: {
-        command: 'vp lint',
+      test: {
+        command: 'vp test',
+        // dependsOn: ['@thazstack/form-util#build'],
+      },
+      check: {
+        command: 'vp check',
         dependsOn: ['@thazstack/form-util#build'],
       },
     },
@@ -51,5 +57,51 @@ export default defineConfig({
       typeAware: true,
       typeCheck: true,
     },
+  },
+  test: {
+    coverage: {
+      enabled: true,
+      include: ['src/**/*.{ts,tsx}'],
+      provider: 'istanbul',
+      thresholds: {
+        branches: 80,
+        functions: 80,
+        lines: 80,
+        statements: 80,
+      },
+    },
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: 'node',
+          include: ['test/**/*.node.test.ts'],
+        },
+      },
+      {
+        extends: true,
+        test: {
+          include: ['test/**/*.browser.test.{ts,tsx}'],
+          browser: {
+            enabled: true,
+            provider: playwright(),
+            instances: [
+              { name: 'browser-chromium', browser: 'chromium' },
+              { name: 'browser-firefox', browser: 'firefox' },
+            ],
+          },
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'types',
+          include: ['test/**/*.test-d.{ts,tsx}'],
+          typecheck: {
+            enabled: true,
+          },
+        },
+      },
+    ],
   },
 });
